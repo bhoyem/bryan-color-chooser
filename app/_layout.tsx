b1dead43a7dcb158db2f8bd702eeca655acf7737
Bryan Hoyem <bjhoyem@gmail.com>
2026-04-20 15:57:37 -0500

    Adding SafeAreaView

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+
+<Stack.Screen name='index' options={{ headerTitle: "Home" }} />
+<Stack.Screen name='about' options={{ headerTitle: "About" }} />
+
+
+  </Stack>;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Edit app/index.tsx to edit this screen</Text>
+      <Link href="/about">Go to about page</Link>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "lightblue",
+    borderColor: "red",
+    borderWidth: 4,
+  },
+});
